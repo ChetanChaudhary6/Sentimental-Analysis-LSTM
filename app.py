6a4f0e2175654e63651d755377d9c78f0f1b2318
ChetanChaudhary6 <chetanchaudhary015@gmail.com>
2024-01-23 21:49:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,14 +5,14 @@ import tensorflow as tf
 # import re
 from numpy import array
 
-from keras.preprocessing.text import one_hot, Tokenizer
+from tensorflow.keras.preprocessing import one_hot, Tokenizer
 from tensorflow.keras.models import Sequential, load_model
 from tensorflow.keras.layers import LSTM
 from tensorflow.keras.layers import Activation, Dropout, Dense
 from tensorflow.keras.layers import Flatten, GlobalMaxPooling1D, Embedding, Conv1D, LSTM
 from sklearn.model_selection import train_test_split
 from flask import Flask, request, jsonify, render_template
-from keras_preprocessing.sequence import pad_sequences
+from tensorflow.keras.preprocessing import pad_sequences
 # import nltk
 # nltk.download('stopwords')
 from keras_preprocessing.text import tokenizer_from_json
